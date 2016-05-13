2fd9f12544f0ec1a60cb724a973a02d7895d0235
linwCoder <770601773@qq.com>
2016-05-13 23:56:17 +0800

    13-QQ聊天

---
+++ 13-QQcell/13-QQcell/Class/Controller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  13-QQcell
+//
+//  Created by 林威 on 16/5/13.
+//  Copyright © 2016年 林威. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
